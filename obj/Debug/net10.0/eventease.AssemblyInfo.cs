8c4ea423e28d42907f22e97dd275bdf79d5c1153
Critoferlee <cristoferlee01@gmail.com>
2026-05-01 16:54:13 -0600

    Merge remote and local

--- obj/Debug/net10.0/eventease.AssemblyInfo.cs
+++ obj/Debug/net10.0/eventease.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eventease")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b3e4645756490ecaca768b5e2846ca1fc5cec4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("eventease")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eventease")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
